03e32112851f036578904e964bff7fe4700b772a
Karen Pacheco <pachecovejar.k@gmail.com>
2018-11-21 17:39:25 -0300

    add evento

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import UserList from '../components/UserList';
+import UserForm from '../components/UserForm';
+import logo from '../assets/logo.svg';
+import '../assets/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        {id: 1, name: "name example", email: "test@email.com"},
+        {id: 2, name: "name two example", email: "test2@email.com"}
+      ]
+    };
+  }
+
+  handlesOnAddUser (event) {
+    event.preventDefault();
+    let user = {
+      name: event.target.name.value,
+      email: event.target.email.value
+    };
+    this.setState({
+      users: this.state.users.concat([user])
+    });
+  }
+
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Bienvenido a React</h2>
+        </div>
+        <div>
+          <p><strong>Añade usuarios</strong></p>
+          <UserList users={this.state.users}  />
+          <UserForm onAddUser={this.handlesOnAddUser.bind(this)} />
+        </div>
+      </div>
     );
   }
 }
